c296807dfc916de1abc48c0b7248e49dec3d90a9
unknown <cnewdorf@knights.ucf.edu>
2020-10-25 23:00:00 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,13 +8,24 @@ public class PlayerScript : MonoBehaviour
     private Rigidbody2D rd2d;
     public float speed;
     public Text score;
+    public Text lives;
+    public Text end;
     private int scoreValue = 0;
+    private int livesValue = 3;
+    public bool level = false;
+    public AudioSource musicSource;
+    public AudioClip music1;
+    public bool playingMusic1 = false;
+    public AudioClip victory;
+    public bool playingVictory = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        score.text = "Score: " + scoreValue.ToString();
+        lives.text = "Lives: " + livesValue.ToString();
+        end.text = "";
     }
 
     // Update is called once per frame
@@ -22,11 +33,26 @@ public class PlayerScript : MonoBehaviour
     {
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
-        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * 0));
         if (Input.GetKey("escape"))
             {
             Application.Quit();
             }
+    }
+
+    void Update(){
+        if (scoreValue < 8 && playingMusic1 == false){
+            musicSource.clip = music1;
+            musicSource.Play();
+            musicSource.loop = true;
+            playingMusic1 = true;
+        }
+        if (scoreValue == 8 && playingVictory == false){
+            musicSource.clip = victory;
+            musicSource.Play();
+            musicSource.loop = false;
+            playingVictory = true;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -34,8 +60,27 @@ public class PlayerScript : MonoBehaviour
         if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            score.text = "Score: " + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+        }
+        if (scoreValue == 8)
+        {
+            end.text = "You win! (Game created by Casey Newdorf)";
+        }
+        if (livesValue == 0){
+            end.text = "You lose! Try again.";
+        }
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            lives.text = "Lives: " + livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+        if (scoreValue == 4 && level == false){
+            transform.position = new Vector2(00.0f, 50.0f);
+            livesValue = 3;
+            lives.text = "Lives: " + livesValue.ToString();
+            level = true;
         }
     }
 
@@ -45,7 +90,7 @@ public class PlayerScript : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                rd2d.AddForce(new Vector2(0, 4), ForceMode2D.Impulse);
             }
         }
     }
